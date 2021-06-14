fa18049d3cae6e05c069863943c7abc78e39288f
Swathisathyan <82638675+Swathisathyan@users.noreply.github.com>
2021-06-14 14:20:09 +0530

    max.c
    This is a c program using the function to return the largest integer in a 1D array

--- solns.c
+++ solns.c
@@ -1,2 +1,25 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int a[],int n);
+int main()
+{int n,a[n];
+int largest=max(a,n);
+printf("\nThe largest numberin this integer array is %d",largest);
+}
+max(int a[],int n)
+{
+	int largest;
+printf("enter the size of the array");
+scanf("%d",&n);
+printf("enter the array elements :\n");
+for(int i=0;i<n;i++){
+	scanf("%d",&a[i]);}
+	largest=a[0];
+
+for(int i=0;i<n;i++){
+	if(a[i]>largest)
+	{
+	largest=a[i];
+	}}
+	return largest;
+}
